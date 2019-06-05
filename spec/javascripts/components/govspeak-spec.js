760ec71c57c6034db1ec309977954447cd773365
kr8n3r <jani@kraner.dev>
2019-06-05 17:12:06 +0100

    Update govspeak and youtube enhancement tests

--- spec/javascripts/components/govspeak-spec.js
+++ spec/javascripts/components/govspeak-spec.js
@@ -1,42 +1,74 @@
 /* eslint-env jasmine, jquery */
 /* global GOVUK */
 
-describe('Govspeak component', function () {
+describe('Govspeak', function () {
   var govspeakModule = new GOVUK.Modules.Govspeak()
 
-  it('embeds youtube videos', function () {
-    var $element = $(
-      '<div class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
-        '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
-      '<div>'
-    )
-    govspeakModule.start($element)
-    expect($element.find('.media-player').length).toBe(1)
+  describe('youtube enhancement', function () {
+    var container
+
+    beforeEach(function () {
+      // For some reason, JSON.parse on the cookie works in the browser, but fails in Jasmine tests.
+      // It seems to be due to extra escaping of quotes when the code is run in the tests, which means JSON.parse doesn't
+      // work as expected. So we'll stub this value instead.
+      spyOn(JSON, 'parse').and.returnValue({
+        'essential': true,
+        'settings': true,
+        'usage': true
+      })
+      window.GOVUK.cookie('cookie_policy', null)
+    })
+
+    afterEach(function () {
+      document.body.removeChild(container)
+    })
+
+    it('embeds youtube videos', function () {
+      container = document.createElement('div')
+      container.innerHTML =
+        '<div class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
+          '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
+        '<div>'
+      document.body.appendChild(container)
+
+      var element = document.querySelector('.gem-c-govspeak')
+      govspeakModule.start($(element))
+
+      expect(document.querySelectorAll('.youtube-video-container').length).toBe(1)
+      expect(document.querySelectorAll("[id='0XpAtr24uUQ']").length).toBe(1)
+    })
+
+    it('allows disabling embeds of youtube videos', function () {
+      container = document.createElement('div')
+      container.innerHTML =
+        '<div class="gem-c-govspeak govuk-govspeak disable-youtube" data-module="govspeak">' +
+          '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
+        '<div>'
+      document.body.appendChild(container)
+
+      var element = document.querySelector('.gem-c-govspeak')
+      govspeakModule.start($(element))
+
+      expect(document.querySelectorAll('.youtube-video-container').length).toBe(0)
+      expect(document.querySelectorAll("[id='0XpAtr24uUQ']").length).toBe(0)
+    })
   })
 
-  it('embeds barcharts', function () {
-    var $element = $(
-      '<div id="govspeak-barchart" class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
-        '<table class="js-barchart-table mc-auto-outdent">' +
-          '<tbody>' +
-            '<tr>' +
-              '<td>row 1</td><td>10</td>' +
-            '</tr>' +
-          '</tbody>' +
-        '</table>' +
-      '<div>'
-    )
-    govspeakModule.start($element)
-    expect($element.find('.mc-chart').length).toBe(1)
-  })
-
-  it('allows disabling embeds of youtube videos', function () {
-    var $element = $(
-      '<div class="gem-c-govspeak govuk-govspeak disable-youtube" data-module="govspeak">' +
-        '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
-      '<div>'
-    )
-    govspeakModule.start($element)
-    expect($element.find('.media-player').length).toBe(0)
+  describe('barchart enhancement', function () {
+    it('embeds barcharts', function () {
+      var $element = $(
+        '<div id="govspeak-barchart" class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
+          '<table class="js-barchart-table mc-auto-outdent">' +
+            '<tbody>' +
+              '<tr>' +
+                '<td>row 1</td><td>10</td>' +
+              '</tr>' +
+            '</tbody>' +
+          '</table>' +
+        '<div>'
+      )
+      govspeakModule.start($element)
+      expect($element.find('.mc-chart').length).toBe(1)
+    })
   })
 })
--- spec/javascripts/govuk_publishing_components/lib/govspeak/youtube-link-enhancement-spec.js
+++ spec/javascripts/govuk_publishing_components/lib/govspeak/youtube-link-enhancement-spec.js
@@ -1,6 +1,10 @@
 describe('Youtube link enhancement', function () {
   describe('default behaviour and no consent', function () {
+    var container
+
     beforeEach(function () {
+      container = document.createElement('div')
+
       // For some reason, JSON.parse on the cookie works in the browser, but fails in Jasmine tests.
       // It seems to be due to extra escaping of quotes when the code is run in the tests, which means JSON.parse doesn't
       // work as expected. So we'll stub this value instead.
@@ -12,71 +16,94 @@ describe('Youtube link enhancement', function () {
       window.GOVUK.cookie('cookie_policy', null)
     })
 
-    it('replaces a link and it\'s container with a media-player embed', function () {
-      var $element = $('<div><p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p></div>')
-      var $toReplace = $element.find('p, a')
+    afterEach(function() {
+      document.body.removeChild(container)
+    })
 
-      var enhancement = new GOVUK.GovspeakYoutubeLinkEnhancement($element)
+    it('replaces a link and it\'s container with a media-player embed', function () {
+      container.innerHTML =
+        '<div class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
+          '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
+        '<div>'
+      document.body.appendChild(container)
+
+      var element = document.querySelectorAll('.gem-c-govspeak')
+      var enhancement = new GOVUK.GovspeakYoutubeLinkEnhancement($(element))
       enhancement.init()
-      expect($element.find('.media-player .video').length).toBe(1)
-      expect($element.find($toReplace).length).toBe(0)
+
+      expect(document.querySelectorAll('.youtube-video-container [id="0XpAtr24uUQ"]').length).toBe(1)
+      expect(document.querySelectorAll('[id="0XpAtr24uUQ"] p, [id="0XpAtr24uUQ"] a').length).toBe(0)
     })
 
     it('doesn\'t replace non Youtube links', function () {
-      var $element = $('<div><p><a href="https://www.gov.uk/">GOV.UK</a></p></div>')
-      var $toReplace = $element.find('p, a')
+      container.innerHTML =
+        '<div class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
+          '<p><a href="https://www.gov.uk">GOV.UK</a></p>' +
+        '<div>'
+      document.body.appendChild(container)
 
-      var enhancement = new GOVUK.GovspeakYoutubeLinkEnhancement($element)
+      var element = document.querySelectorAll('.gem-c-govspeak')
+      var enhancement = new GOVUK.GovspeakYoutubeLinkEnhancement($(element))
       enhancement.init()
 
-      expect($element.find('.media-player .video').length).toBe(0)
-      expect($element.find($toReplace).length).toBe(2)
+      expect(document.querySelectorAll('.youtube-video-container iframe').length).toBe(0)
+      expect(document.querySelectorAll('.gem-c-govspeak p, .gem-c-govspeak a').length).toBe(2)
     })
 
     it('doesn\'t replace links marked not to embed', function () {
-      var $element = $('<div><p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ" data-youtube-player="off">Agile at GDS</a></p></div>')
-      var $toReplace = $element.find('p, a')
+      container.innerHTML = '<div class="gem-c-govspeak"><p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ" data-youtube-player="off">Agile at GDS</a></p></div>'
+      document.body.appendChild(container)
 
-      var enhancement = new GOVUK.GovspeakYoutubeLinkEnhancement($element)
+      var element = document.querySelectorAll('.gem-c-govspeak')
+      var enhancement = new GOVUK.GovspeakYoutubeLinkEnhancement($(element))
       enhancement.init()
 
-      expect($element.find('.media-player .video').length).toBe(0)
-      expect($element.find($toReplace).length).toBe(2)
+      expect(document.querySelectorAll('.youtube-video-container [id="0XpAtr24uUQ"]').length).toBe(0)
+      expect(document.querySelectorAll('.gem-c-govspeak p, .gem-c-govspeak a').length).toBe(2)
+    })
+  })
+
+  describe('parseVideoId', function () {
+    it('returns an id for a youtube.com video URL', function () {
+      var url = 'https://www.youtube.com/watch?v=0XpAtr24uUQ'
+      var id = GOVUK.GovspeakYoutubeLinkEnhancement.parseVideoId(url)
+
+      expect(id).toEqual('0XpAtr24uUQ')
     })
 
-    describe('parseVideoId', function () {
-      it('returns an id for a youtube.com video URL', function () {
-        var url = 'https://www.youtube.com/watch?v=0XpAtr24uUQ'
-        var id = GOVUK.GovspeakYoutubeLinkEnhancement.parseVideoId(url)
+    it('returns an id for a youtu.be video URL', function () {
+      var url = 'https://youtu.be/0XpAtr24uUQ'
+      var id = GOVUK.GovspeakYoutubeLinkEnhancement.parseVideoId(url)
 
-        expect(id).toEqual('0XpAtr24uUQ')
-      })
+      expect(id).toEqual('0XpAtr24uUQ')
+    })
 
-      it('returns an id for a youtu.be video URL', function () {
-        var url = 'https://youtu.be/0XpAtr24uUQ'
-        var id = GOVUK.GovspeakYoutubeLinkEnhancement.parseVideoId(url)
+    it('doesn\'t return an id for a Youtube non video', function () {
+      var url = 'https://www.youtube.com/channel/UCSNK6abAoM6Kj0SkHOStNLg'
+      var id = GOVUK.GovspeakYoutubeLinkEnhancement.parseVideoId(url)
 
-        expect(id).toEqual('0XpAtr24uUQ')
-      })
+      expect(id).not.toBeDefined()
+    })
 
-      it('doesn\'t return an id for a Youtube non video', function () {
-        var url = 'https://www.youtube.com/channel/UCSNK6abAoM6Kj0SkHOStNLg'
-        var id = GOVUK.GovspeakYoutubeLinkEnhancement.parseVideoId(url)
+    it('doesn\'t return an id for a non youtube link', function () {
+      var url = 'https://www.gov.uk'
+      var id = GOVUK.GovspeakYoutubeLinkEnhancement.parseVideoId(url)
 
-        expect(id).not.toBeDefined()
-      })
-
-      it('doesn\'t return an id for a non youtube link', function () {
-        var url = 'https://www.gov.uk'
-        var id = GOVUK.GovspeakYoutubeLinkEnhancement.parseVideoId(url)
-
-        expect(id).not.toBeDefined()
-      })
+      expect(id).not.toBeDefined()
     })
   })
 
   describe('cookie consent for campaign true', function () {
+    var container
+
     beforeEach(function () {
+      container = document.createElement('div')
+      container.innerHTML =
+        '<div class="gem-c-govspeak govuk-govspeak" data-module="govspeak">' +
+          '<p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p>' +
+        '<div>'
+      document.body.appendChild(container)
+
       // For some reason, JSON.parse on the cookie works in the browser, but fails in Jasmine tests.
       // It seems to be due to extra escaping of quotes when the code is run in the tests, which means JSON.parse doesn't
       // work as expected. So we'll stub this value instead.
@@ -86,14 +113,17 @@ describe('Youtube link enhancement', function () {
       window.GOVUK.cookie('cookie_policy', "{\"campaigns\":true}")
     })
 
+    afterEach(function() {
+      document.body.removeChild(container)
+    })
+
     it('replaces a link and its container with a media-player embed when campaign cookies are turned on', function () {
-      var $element = $('<div><p><a href="https://www.youtube.com/watch?v=0XpAtr24uUQ">Agile at GDS</a></p></div>')
-      var $toReplace = $element.find('p, a')
-      var enhancement = new GOVUK.GovspeakYoutubeLinkEnhancement($element)
+      var element = document.querySelectorAll('.gem-c-govspeak')
+      var enhancement = new GOVUK.GovspeakYoutubeLinkEnhancement($(element))
       enhancement.init()
 
-      expect($element.find('.media-player .video').length).toBe(1)
-      expect($element.find($toReplace).length).toBe(0)
+      expect(document.querySelectorAll('.youtube-video-container [id="0XpAtr24uUQ"]').length).toBe(1)
+      expect(document.querySelectorAll('[id="0XpAtr24uUQ"] p, [id="0XpAtr24uUQ"] a').length).toBe(0)
     })
   })
 
@@ -108,7 +138,7 @@ describe('Youtube link enhancement', function () {
       var enhancement = new GOVUK.GovspeakYoutubeLinkEnhancement($element)
       enhancement.init()
 
-      expect($element.find('.media-player .video').length).toBe(0)
+      expect($element.find('.youtube-video-container iframe').length).toBe(0)
       expect($element.find($toReplace).length).toBe(2)
     })
   })
