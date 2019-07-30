4ac0874f13f0b0cc0c40d4abaec9e0af455fa4f7
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-07-30 14:38:57 +0000

    try different way of implementing player

--- app/assets/javascripts/govuk_publishing_components/lib/govspeak/youtube-link-enhancement.js
+++ app/assets/javascripts/govuk_publishing_components/lib/govspeak/youtube-link-enhancement.js
@@ -14,6 +14,95 @@
 
     var $youtubeLinks = this.$element.querySelectorAll('a[href*="youtube.com"], a[href*="youtu.be"]')
 
+    for (var i = 0; i < $youtubeLinks.length; ++i) {
+      var $youtubeLink = $youtubeLinks[i]
+      var elementId = YoutubeLinkEnhancement.nextId()
+      var videoId = YoutubeLinkEnhancement.parseVideoId($youtubeLink.getAttribute('href'))
+
+      if (!this.hasDisabledEmbed($youtubeLink) && videoId) {
+        var parentDiv = document.createElement('div')
+        parentDiv.classList += 'gem-c-govspeak__youtube-video'
+
+        var iframe = document.createElement("iframe")
+        iframe.id = elementId
+        iframe.setAttribute('src', 'https://www.youtube-nocookie.com/embed/' + videoId + '?enablejsapi=1&origin= ' + window.location.origin + '&rel=0&disablekb=1&modestbranding=1')
+        iframe.setAttribute('title', $youtubeLink.textContent + ' (video)')
+        iframe.setAttribute('data-video-id', videoId)
+        iframe.setAttribute('frameborder', 0)
+        iframe.setAttribute('allowfullscreen', 1)
+        iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture')
+
+        var parentPara = $youtubeLink.parentNode
+        var parentContainer = parentPara.parentNode
+
+        parentDiv.appendChild(iframe)
+        parentContainer.replaceChild(parentDiv, parentPara)
+
+        window.onYouTubeIframeAPIReady = function() {
+          new YT.Player(elementId, {});
+        }
+      }
+    }
+
+    if (!this.youtubeApiInserted) {
+      var tag = document.createElement('script')
+      tag.id = 'iframe-demo'
+      tag.src = 'https://www.youtube.com/iframe_api'
+      var firstScriptTag = document.getElementsByTagName('script')[0]
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+      this.youtubeApiInserted = true
+    }
+  }
+
+  YoutubeLinkEnhancement.prototype.campaignCookiesAllowed = function () {
+    var cookiePolicy = window.GOVUK.getConsentCookie()
+    return cookiePolicy !== null ? cookiePolicy.campaigns : true
+  }
+
+  YoutubeLinkEnhancement.prototype.hasDisabledEmbed = function ($link) {
+    return $link.getAttribute('data-youtube-player') === 'off'
+  }
+
+  // This is a public class method so it can be used outside of this embed to
+  // check that user input for videos will be supported in govspeak
+  YoutubeLinkEnhancement.parseVideoId = function (url) {
+    var parts
+
+    if (url.indexOf('youtube.com') > -1) {
+      var params = {}
+      parts = url.split('?')
+      if (parts.length === 1) {
+        return
+      }
+      parts = parts[1].split('&')
+      for (var i = 0; i < parts.length; i++) {
+        var part = parts[i].split('=')
+        params[part[0]] = part[1]
+      }
+      return params.v
+    }
+
+    if (url.indexOf('youtu.be') > -1) {
+      parts = url.split('/')
+      return parts.pop()
+    }
+  }
+
+  YoutubeLinkEnhancement.nextId = function () {
+    this.embedCount = this.embedCount || 0
+    this.embedCount += 1
+    return 'youtube-' + this.embedCount
+  }
+
+
+
+  /*YoutubeLinkEnhancement.prototype.init = function () {
+    if (!this.campaignCookiesAllowed()) {
+      return
+    }
+
+    var $youtubeLinks = this.$element.querySelectorAll('a[href*="youtube.com"], a[href*="youtu.be"]')
+
     if ($youtubeLinks.length > 0) {
       YoutubeLinkEnhancement.insertApiScript()
     }
@@ -141,7 +230,7 @@
     for (var i = 0; i < YoutubeLinkEnhancement.queuedInserts.length; i++) {
       YoutubeLinkEnhancement.queuedInserts[i].call()
     }
-  }
+  }*/
 
   GOVUK.GovspeakYoutubeLinkEnhancement = YoutubeLinkEnhancement
 })()
