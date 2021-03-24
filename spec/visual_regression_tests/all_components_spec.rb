47c0d77cf7724b5529698dedfabf078ae4310590
Ian James <ian.james@digital.cabinet-office.gov.uk>
2021-03-24 11:13:05 +0000

    Add public component preview

--- spec/visual_regression_tests/all_components_spec.rb
+++ spec/visual_regression_tests/all_components_spec.rb
@@ -12,27 +12,30 @@ describe "visual regression test runner Percy", visual_regression: true do
     component_links = find("#list-all-components-in-the-gem")
       .all("a")
       .map do |link|
-        URI(link[:href])
+        URI(link[:href] + "/preview")
       end
+
+    # The public layout component has two previews - one with multiple versions
+    # as part of the component guide, and another one at `/public`.
+    component_links << URI("/public")
 
     component_links.each do |link|
       skip_as_govspeak_times_out = [
-        "/component-guide/govspeak",
+        "/component-guide/govspeak/preview",
       ]
 
       skip_non_visible_components = [
-        "/component-guide/meta_tags",
-        "/component-guide/machine_readable_metadata",
-        "/component-guide/admin_analytics",
-        "/component-guide/google_tag_manager_script",
-        "/component-guide/layout_for_admin",
+        "/component-guide/meta_tags/preview",
+        "/component-guide/machine_readable_metadata/preview",
+        "/component-guide/admin_analytics/preview",
+        "/component-guide/google_tag_manager_script/preview",
+        "/component-guide/layout_for_admin/preview",
       ]
 
       all_components_to_skip = skip_as_govspeak_times_out + skip_non_visible_components
 
       next if all_components_to_skip.include?(link.path)
-
-      visit("#{link}/preview")
+      visit(link)
       name = title.gsub(/(: Default|) preview - Component Guide/, "")
       page.find(:css, "body > #wrapper", wait: 10)
       Percy.snapshot(page, name: name)
